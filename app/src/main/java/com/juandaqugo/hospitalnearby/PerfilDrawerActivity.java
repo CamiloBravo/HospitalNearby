6a39db2360ff84c2f608ddfeb6ade0c0b2e3240a
CamiloBravo <camilobc89@gmail.com>
2017-04-26 10:40:54 -0500

    perfil mejorado y cambio en los settings de la parte superior derecha (todavia no funcionan), ademas ya sale la lista de hospitales y botones, falta darles accion.

--- app/src/main/java/com/juandaqugo/hospitalnearby/PerfilDrawerActivity.java
+++ app/src/main/java/com/juandaqugo/hospitalnearby/PerfilDrawerActivity.java
@@ -75,14 +75,14 @@ public class PerfilDrawerActivity extends AppCompatActivity
         tcedula_perfil.setText(documento);
         teps_perfil.setText(EPS);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
